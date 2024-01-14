2e0e0b582fbdb9bfa38724172d95dbcd4236d1ff
mpirrocco <student@dospasosmas.uy>
2024-01-14 11:14:32 -0300

    change port

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.get('/', (req, res) => {
   res.send({ bye: 'buddy' })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Node is listening on port: ${PORT}`))
